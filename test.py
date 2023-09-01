b9b4745f13c14da994927e23d80ffc69d39ce468
duoze <duoze@git.com>
2023-09-01 18:40:53 +0800

    09.01 done

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
 import torch
 from torch import nn
 
-n_train = 5
-x_train, _ = torch.sort(torch.rand(n_train) * 5)
-
-def f(x):
-    return 2 * torch.sin(x) + x**0.8
-
-y_train = f(x_train) + torch.normal(0.0, 0.5, (n_train,))
-x_test = torch.arange(0, 3, 1)
-y_truth = f(x_test)
-n_test = len(x_test)
-
-print("train_set")
-print(x_train)
-print(y_train)
-print("query_set")
-print(x_test)
+# n_train = 5
+# x_train, _ = torch.sort(torch.rand(n_train) * 5)
+#
+# def f(x):
+#     return 2 * torch.sin(x) + x**0.8
+#
+# y_train = f(x_train) + torch.normal(0.0, 0.5, (n_train,))
+# x_test = torch.arange(0, 3, 1)
+# y_truth = f(x_test)
+# n_test = len(x_test)
+#
+# print("train_set")
+# print(x_train)
+# print(y_train)
+# print("query_set")
+# print(x_test)
 
 # X_repeat = x_test.repeat_interleave(n_train).reshape((-1, n_train))
 # attention_weights = nn.functional.softmax(-(X_repeat - x_train)**2 / 2, dim=1)
@@ -26,16 +26,16 @@ print(x_test)
 # print(attention_weights)
 # print(y_hat)
 
-X_tile = x_train.repeat((n_train, 1))
-Y_tile = y_train.repeat((n_train, 1))
-keys = X_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
-values = Y_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
-print("tile")
-print(X_tile)
-print(Y_tile)
-print("keys-values")
-print(keys)
-print(values)
+# X_tile = x_train.repeat((n_train, 1))
+# Y_tile = y_train.repeat((n_train, 1))
+# keys = X_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+# values = Y_tile[(1 - torch.eye(n_train)).type(torch.bool)].reshape((n_train, -1))
+# print("tile")
+# print(X_tile)
+# print(Y_tile)
+# print("keys-values")
+# print(keys)
+# print(values)
 
 
 # 小批量矩阵乘法
@@ -52,3 +52,4 @@ print(values)
 # print(values)
 # output = torch.bmm(weights.unsqueeze(1), values.unsqueeze(-1))
 # print(output)
+
